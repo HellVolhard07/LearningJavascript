eee173a5ed14aa3c3e2d7903fc6dffcfba1bb35f
HellVolhard07 <itsmevasu84@gmail.com>
2022-03-31 23:50:55 +0530

    Hooks and errors

--- React/first-app/src/Home.js
+++ React/first-app/src/Home.js
@@ -1,16 +1,37 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import BlogList from "./BlogList"
 
 const Home = () => {
-  const [name, setName] = useState("Mario")
-  const handleClick = () => {
-    setName("Yoshi")
-  }
+  const [blogs, setBlogs] = useState(null)
+  const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch the data from that resourse")
+          }
+          return res.json()
+        })
+        .then((data) => {
+          setError(null)
+          setBlogs(data)
+          setIsPending(false)
+        })
+        .catch((err) => {
+          setError(err.message)
+          setIsPending(false)
+        })
+    }, 1000)
+  }, [])
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{name}</p>
-      <button onClick={handleClick}>Click me</button>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   )
 }
